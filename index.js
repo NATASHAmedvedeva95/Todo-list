01af098fc04811336b99b6d39f7831fea3b277d2
Natalia <92246160+NATASHAmedvedeva95@users.noreply.github.com>
2022-06-27 18:08:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -69,8 +69,9 @@ function addHeader() {
     while(ul.firstElementChild){
       ul.firstElementChild.remove();
     }
+     let data = document.querySelector('data');
      const noteDataJson = JSON.stringify(data);
-  localStorage.setItem('todos', noteDataJson );
+     localStorage.setItem('todos', noteDataJson );
     // 2 способ убирает по очереди
     // addUl(tasks, tasksWrapper.firstElementChild).remove();
   });
